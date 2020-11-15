d9b579277cbfb04078ceff0cc15ea75827e72b40
emySchoettel <e.schoettel@ludus-academie.com>
2020-11-15 14:45:32 +0100

    Type : array

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    public string nameOne = "circle"; 
+    public string nameTwo = "square";
+    public string nameThree = "triangle"; 
+    public string nameFour = "octagon"; 
+
+    public string[] shapes = { "circle", "square", "triangle", "octagon"}; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for(int i = 0; i < shapes.Length; i++)
+        {
+            shapes[i] = shapes[i].ToUpper(); 
+            print(shapes[i]);
+        }
+        
+        // nameOne = nameOne.ToUpper(); 
+        // nameTwo = nameTwo.ToUpper(); 
+        // nameThree = nameThree.ToUpper(); 
+        // nameFour = nameFour.ToUpper(); 
+
+        // print(nameOne);
+        // print(nameTwo);
+        // print(nameThree);
+        // print(nameFour);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
