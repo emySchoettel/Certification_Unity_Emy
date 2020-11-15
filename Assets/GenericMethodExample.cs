deeae6c095437dac8ba8ccccd4088f8b3e1197cf
emySchoettel <e.schoettel@ludus-academie.com>
2020-11-15 14:35:15 +0100

    Cours sur les types

---
+++ Assets/GenericMethodExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericMethodExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float newNumber = 5.3f; 
+        Vector3 newVector = new Vector3(1,3,2);
+        Evaluate("Hello world"); 
+        Evaluate(10);
+        Evaluate(newNumber);
+        Evaluate(newVector);
+    }
+
+    private void Evaluate<T> (T suppliedValue)
+    {
+        Debug.LogFormat("the Type of {0} is {1}", suppliedValue, typeof(T));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SampleScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+//using UnityEngine;
+namespace BeyondTheBasics.Gameplay{
+public class SampleScript : UnityEngine.MonoBehaviour
+{
+    public int numberToDisplay;
+    public int numberToMultiplyBy; 
+    private void SayHelloWorld()
+    {
+        var randomNumber = UnityEngine.Random.Range(0, numberToDisplay);
+        var displayText = "Hello World"; 
+        for(int i = 0; i < randomNumber; i++)
+        {
+            print(displayText);
+        }
+    }
+
+    private void AnonymousTypeExample()
+    {
+        //var enemy = new { name = "Monstrer", hitpoints = 100};
+
+        var enemies = new[] {
+            new { name = "Monster", hitPoints = 100},
+            new { name = "Goblin", hitPoints = 200},
+            new { name = "Beast", hitPoints = 300}
+        };
+
+        var enemiesQuery = 
+            from enemy in enemies
+            orderby enemy.name
+            select enemy; 
+
+        foreach(var enemy in enemiesQuery)
+        {
+            print(enemy.name);
+        }
+
+    }
+
+    private void MultiplyNumber(int numberToMultiply)
+    {
+        #region essai
+        int product = numberToMultiplyBy * numberToMultiply; 
+        print(product);
+        #endregion
+    }
+    // Start is called before the first frame update
+   private void Start()
+    {
+        AnonymousTypeExample();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+}
