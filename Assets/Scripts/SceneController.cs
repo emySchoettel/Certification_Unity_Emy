b0de89319efcacb16f14d3636a504475fcbb7021
emySchoettel <e.schoettel@ludus-academie.com>
2020-11-15 14:59:18 +0100

    Dictionnaire

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,36 +5,58 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
 
-    public string nameOne = "circle"; 
-    public string nameTwo = "square";
-    public string nameThree = "triangle"; 
-    public string nameFour = "octagon"; 
-
-    public string[] shapes = { "circle", "square", "triangle", "octagon"}; 
+    public List<Shape> gameShapes; 
+    public Dictionary<string, Shape> shapeDictionnary; 
     // Start is called before the first frame update
     void Start()
     {
-
-        for(int i = 0; i < shapes.Length; i++)
+        shapeDictionnary = new Dictionary<string, Shape>(); 
+        foreach(Shape shape in gameShapes)
         {
-            shapes[i] = shapes[i].ToUpper(); 
-            print(shapes[i]);
+            shapeDictionnary.Add(shape.Name, shape);
         }
-        
-        // nameOne = nameOne.ToUpper(); 
-        // nameTwo = nameTwo.ToUpper(); 
-        // nameThree = nameThree.ToUpper(); 
-        // nameFour = nameFour.ToUpper(); 
+    }
 
-        // print(nameOne);
-        // print(nameTwo);
-        // print(nameThree);
-        // print(nameFour);
+    private void SetRedByName(string shapeName)
+    {
+        shapeDictionnary[shapeName].SetColor(Color.red); 
+    }
+
+    private void FindExample()
+    {
+        //foreach shape trouver là où s.Name == octagon
+        Shape octagon = gameShapes.Find(s => s.Name == "Octagon");
+        octagon.SetColor(Color.red);
+    }
+    private void ListExample()
+    {
+        string[] shapes = { "circle", "square", "triangle", "octagon"}; 
+        List<string> moreShapes;
+
+        moreShapes = new List<string> { "circle", "square", "triangle", "octagon"}; 
+
+        moreShapes.Add("rectangle");
+        moreShapes.Insert(2, "diamont");
+        moreShapes.Sort();
+
+        for(int i = 0; i < moreShapes.Count; i++)
+        {
+            moreShapes[i] = moreShapes[i].ToUpper();
+            print(moreShapes[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SetRedByName("Square");
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            SetRedByName("Circle");
+        }
         
     }
 }
