0645dc0da57332776de26e4550f51c06ce6dc51e
emySchoettel <e.schoettel@ludus-academie.com>
2020-11-15 15:06:35 +0100

    Stack et Queue

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,21 +7,35 @@ public class SceneController : MonoBehaviour
 
     public List<Shape> gameShapes; 
     public Dictionary<string, Shape> shapeDictionnary; 
+    public Queue<Shape> shapeQueue;
+
+    public Stack<Shape> shapeStack; 
     // Start is called before the first frame update
     void Start()
     {
+        shapeStack = new Stack<Shape>(); 
+        shapeQueue = new Queue<Shape>(); 
         shapeDictionnary = new Dictionary<string, Shape>(); 
         foreach(Shape shape in gameShapes)
         {
             shapeDictionnary.Add(shape.Name, shape);
         }
+
+        shapeQueue.Enqueue(shapeDictionnary["Triangle"]);
+        shapeQueue.Enqueue(shapeDictionnary["Square"]);
+        shapeQueue.Enqueue(shapeDictionnary["Octagon"]);
+        shapeQueue.Enqueue(shapeDictionnary["Circle"]);
+
+        shapeStack.Push(shapeDictionnary["Triangle"]);
+        shapeStack.Push(shapeDictionnary["Square"]);
+        shapeStack.Push(shapeDictionnary["Octagon"]);
+        shapeStack.Push(shapeDictionnary["Circle"]);
     }
 
     private void SetRedByName(string shapeName)
     {
         shapeDictionnary[shapeName].SetColor(Color.red); 
     }
-
     private void FindExample()
     {
         //foreach shape trouver là où s.Name == octagon
@@ -45,9 +59,7 @@ public class SceneController : MonoBehaviour
             print(moreShapes[i]);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void DictionnaryExample()
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
@@ -57,6 +69,39 @@ public class SceneController : MonoBehaviour
         {
             SetRedByName("Circle");
         }
-        
+
+    }
+    
+    private void DeQueueExample()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(shapeQueue.Count > 0)
+            {
+                Shape shape = shapeQueue.Dequeue();
+                shape.SetColor(Color.blue);
+            }
+            else
+            {
+                print("La queue est vide");
+            }
+            
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(shapeStack.Count > 0)
+            {
+                Shape shape = shapeStack.Pop();
+                shape.SetColor(Color.green);
+            }
+            else
+            {
+                print("C'est vide !");
+            }
+        }
     }
 }
